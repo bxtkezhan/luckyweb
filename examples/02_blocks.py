7c173ffd96758c9d21fa352740ec603bfc245fde
bxtkezhan <bxtkezhan@qq.com>
2018-12-20 16:15:13 +0800

    edit example 02

--- examples/02_blocks.py
+++ examples/02_blocks.py
@@ -2,9 +2,8 @@ import luckyweb.blocks as B
 
 
 def helloblocks(request, response):
-    params = request.params
-
     html = B.HtmlBlock(title='Hello lucky blocks')
+
     navbar = B.NavbarBlock(
         left_items=[
             {'href': '/', 'text': 'Lucky Web'},
@@ -17,31 +16,67 @@ def helloblocks(request, response):
         ]
     )
 
-    grid_1 = B.GridBlock([1, 3, 7, 1])
-    img = B.ImgBlock('/static/L.png', href='https://github.com/bxtkezhan/luckyweb', _class='')
-    head_1 = B.HeadBlock('<center> 莎士比亚十四行诗 </center>')
-    lis = B.ListBlock([
-        {'href': '#', 'text': 'Let me not to the marriage of true minds - 我绝不承认两颗真心的结合'},
-        {'href': '#', 'text': 'Admit impediments. Love is not love - 有任何障碍。这样的爱不是真爱'},
-        {'href': '#', 'text': 'Which alters when it alteration finds, - 若是遇有变节的机会就改变，'},
-        {'href': '#', 'text': 'Or bends with the remover to remove: - 或是被强势剥离就屈服：'},
-        {'href': '#', 'text': 'O, no! it is an ever-fix`ed mark, - 哦，那不是爱！爱是坚定的烽火，'}])
+    head_p = B.HeadBlock('PBlock')
+    p = B.PBlock('1234567890qwertyuiop ... zxcvbnm')
 
-    grid_2 = B.GridBlock([6, 6])
-    card = B.CardBlock('La La La !!!')
-    head_2 = B.HeadBlock('<center> IRIS Table </center>', head_num=4)
+    head_img = B.HeadBlock('ImgBlock')
+    img = B.ImgBlock(src='https://avatars0.githubusercontent.com/u/20847355?s=460&v=4')
+
+    head_a = B.HeadBlock('ABlock')
+    a = B.ABlock('luckyweb github repository', href='https://github.com/bxtkezhan/luckyweb')
+
+    head_list = B.HeadBlock('ListBlock', center=True)
+    _list = B.ListBlock([
+        dict(href='#', text='aaaaaaaaaaaaaaaaaa'),
+        dict(href='#', text='bbbbbbbbbbbbbbbbbb'),
+        dict(href='#', text='cccccccccccccccccc')])
+
+    head_table = B.HeadBlock('TableBlock', center=True)
     table = B.TableBlock([
-        ['#', '花萼长度', '花萼宽度', '花瓣长度', '花瓣宽度', '属种'],
+        ['#', 'column 1', 'column 2', 'column 3', 'column 4', 'column 5'],
         ['1', '5.2', '4.1', '1.5', '0.1', 'setosa'],
         ['2', '6.0', '2.2', '4.0', '1.0', 'versicolor'],
         ['3', '4.9', '2.5', '4.5', '1.7', 'virginica']])
 
+    head_form = B.HeadBlock('FormBlock', center=True)
+    form = B.FormBlock(action='/', groups=[
+        {'label': 'Username', 'type': 'text', 'placeholder': 'input username', 'name': 'username'},
+        {'label': 'Password', 'type': 'password', 'placeholder': 'input password', 'name': 'password'}])
+
+    head_card = B.HeadBlock('CardBlock', center=True)
+    card = B.CardBlock('Card header')
+
+    head_grid = B.HeadBlock('GridBlock', center=True)
+    grid = B.GridBlock([4, 4, 4])
+
     html([
         navbar,
-        grid_1(
-            ['', img, head_1 + lis, '']),
-        grid_2(
-            [card(head_2 + table)] * 2),
+        head_p,
+        p,
+        '<br><hr color="#F00">',
+        head_img,
+        img,
+        '<br><hr color="#F00">',
+        head_a,
+        a,
+        '<br><hr color="#F00">',
+        head_list,
+        _list,
+        '<br><hr color="#F00">',
+        head_table,
+        table,
+        '<br><hr color="#F00">',
+        head_form,
+        form,
+        '<br><hr color="#F00">',
+        head_card,
+        card([
+            p,
+            img,
+            a]),
+        '<br><hr color="#F00">',
+        head_grid,
+        grid([_list, table, form])
     ])
 
     return html
